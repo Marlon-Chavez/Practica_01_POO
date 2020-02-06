2adeb9b2bc1c6628b4357b6cc32b5603d53a49e0
Marlon-Chavez <60248432+Marlon-Chavez@users.noreply.github.com>
2020-02-05 18:47:48 -0600

    Add files via upload

---
+++ 1.1 Imprimir datos en la pantalla.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+int  main(void)
+{
+    std::cout <<"Hola ESIME Zacatenco"<< std::endl;
+}
+
---
+++ 2.1 Declaracion de una clase.cpp
@@ -0,0 +1,10 @@
+class Gato
+{
+public:
+    unsigned int suEdad;
+    unsigned int suPeso;
+    void Maullar ()
+    {
+        cout << "miauuuu"<< endl;
+    }
+};
